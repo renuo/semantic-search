1e3aee0557d94818791ceef1a09951a6e31e144a
Sami Hindi <sami.hindi@renuo.ch>
2025-05-09 12:17:32 +0200

    Update configuration

--- init.rb
+++ init.rb
@@ -1,16 +1,49 @@
-Redmine::Plugin.register :motivation_center do
-  name 'Motivation Center 🔥'
+require 'dotenv/load'
+require 'ruby/openai'
+
+require 'pathname'
+plugin_root = Pathname.new(__FILE__).dirname
+lib_dir = plugin_root.join('lib')
+$LOAD_PATH.unshift(lib_dir.to_s) unless $LOAD_PATH.include?(lib_dir.to_s)
+
+require_dependency 'semantic_search/issue_hooks'
+require_dependency 'semantic_search/hooks/view_hooks'
+
+Redmine::Plugin.register :semantic_search do
+  name 'Semantic Search'
   author 'Sami Hindi @ Renuo'
-  description 'This is a simple redmine plugin boilerplate to help you get started with your own plugins.'
+  description 'This redmine plugin allows you to search issues using natural language, by storing the issue content in a vector database.'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/renuo/redmine-semantic-search'
+  author_url 'https://github.com/renuo'
 
-  settings default: {'message' => 'Keep up the great work! 💪'}, partial: 'settings/motivation_settings'
+  settings default: {
+    'base_url' => 'https://api.openai.com/v1',
+    'embedding_model' => 'text-embedding-ada-002',
+    'search_limit' => 25,
+    'include_description' => '1',
+    'include_comments' => '1',
+    'include_time_entries' => '1'
+  }, partial: 'settings/semantic_search_settings'
 
-  menu :admin_menu, :motivation_center, {
-    controller: 'motivation',
-    action: 'index'
-  },
-  caption: 'Motivation Center'
+  menu :top_menu, :semantic_search,
+       { controller: 'semantic_search', action: 'index' },
+       caption: :label_semantic_search,
+       if: Proc.new { User.current.logged? && User.current.allowed_to?(:use_semantic_search, nil, global: true) }
+
+  menu :admin_menu, :semantic_search,
+       { controller: 'semantic_search', action: 'settings' },
+       caption: :label_semantic_search_settings
+
+  menu :application_menu, :sync_embeddings,
+       { controller: 'semantic_search', action: 'sync_embeddings' },
+       caption: :button_sync_embeddings,
+       html: { method: :post },
+       if: Proc.new { User.current.admin? }
+
+  project_module :semantic_search do
+    permission :use_semantic_search, { semantic_search: [:index] }
+  end
 end
+
+SemanticSearch::IssueHooks.instance
